432499e8f0bc15c599961b689fbe0ef2b05bd75c
Kyle Johnson <gkjohns@gmail.com>
2023-04-26 15:47:53 -0400

    answer_question function works

--- whippersnapper/prompts.py
+++ whippersnapper/prompts.py
@@ -1,61 +1,88 @@
 from langchain.prompts import PromptTemplate
 
-generate_code_prompt = PromptTemplate(
-    input_variables=['question', 'sample_rows', 'columns', 'df_name'],
+generate_sql_prompt = PromptTemplate(
+    input_variables=['question', 'columns', 'samples', 'table_name'],
     template='''
 # Context
-Your job is to write python code to answers the following question:
+Your job is to write a sql query that answers the following question:
 {question}
 
-Use the sample below and columns to understand the data in the table and the column names. 
-Your query should only use the columns contained in the table. 
-
+Below is a list of columns and their datatypes. Your query should only use the data contained in the table. The table name is `{table_name}`.
 
 # Columns
 {columns}
 
-# Sample rows
-{sample_rows}
+# Samples
+{samples}
 
-If the question is not a question or is answerable with the given columns, say you can't answer the question.
+If the question is not a question or is answerable with the given columns, respond to the best of your ability or say the question can't be answered.
 Do not use columns that aren't in the table.
-Ensure that the code runs and returns the correct output.
-Wrap the code in a function that accepts the dataframe as an argument and prints the result. Call the function on the dataframe `{df_name}`.
+Ensure that the query runs and returns the correct output.
 
-# Code:
+# Query:
 '''
 )
 
-code_rewriter_prompt = PromptTemplate(
-    input_variables=['question', 'code', 'error', 'df_name'],
+sql_rewriter_prompt = PromptTemplate(
+    input_variables=['question', 'columns', 'samples', 'table_name', 'query'],
     template='''
 # Context
-Your job is to identify why a segment of code produced an error, then rewrite it to make it work.
-If the code inticates that the question cannot be answered, say that the question cannot be answered and why
-Wrap the code in a function that accepts the dataframe as an argument and prints the result. Call the function on the dataframe `{df_name}`.
-Wrap all code in triple backticks, like markdown.
+Your job is to identify why a segment of a query produced an error, then rewrite it to make it work.
+If the question cannot be answered with the given dataset, say that the question cannot be answered and briefly explain why.
+Wrap the query in triple backticks, like markdown.
 
 #### EXAMPLE ####
-The code didn't work because it uses a plus sign instead of a minus sign. Here's the updated code:
-
+The query didn't work because you used the column "name", which doesn't exist. It should use the column "first_name" instead.
+The query also is missing a semi-colon at the end. Here's a working version
 
 ```
-a = 1
-b = 2
-
-print(a + b)
+SELECT
+  first_name,
+  SUM(1) AS total_people
+FROM people
+GROUP BY
+  first_name
+LIMIT 10;
 ```
+#################
 
 
 # Initial question
 {question}
 
-# Code
-{code}
+# Table name
+{table_name}
 
-# Error
-{error}
+# Columns
+{columns}
+
+# Samples
+{samples}
+
+# Original query
+{query}
 
 # Rewritten code
+'''
+)
+
+summarizer_prompt = PromptTemplate(
+    input_variables=['question', 'query', 'answer'],
+    template='''
+# Context
+Your job is to summarize the answer to a data question and give a brief explanation of the methodology/approach.
+Be concise and call out any caveats.
+
+
+# Question
+{question}
+
+# Query
+{query}
+
+# Answer
+{answer}
+
+# Summary and methodology
 '''
 )
---
+++ whippersnapper/whippersnapper.py
@@ -0,0 +1,129 @@
+import re
+import sqlite3
+from pathlib import Path
+import pandas as pd
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+
+from prompts import *
+
+
+QUERY_FAILED = '<query_failed>'
+
+class color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+def query_database(db_path, query, max_rows=20):
+    
+    try:
+        with sqlite3.connect(db_path) as connection:
+
+            cursor = connection.cursor()
+            cursor.execute(query)
+
+            rows = cursor.fetchmany(max_rows)   # limit rows
+            columns = tuple([x[0] for x in cursor.description])
+            
+            return pd.DataFrame(rows, columns=columns)
+    
+    except:
+        return '<query_failed>'
+    
+    
+def get_schema(db_path, table_name):
+    
+    command = f"PRAGMA table_info('{table_name}')"
+    
+    with sqlite3.connect(db_path) as connection:
+        cursor = connection.cursor()
+        cursor.execute(command)
+        rows = [row[1:3] for row in cursor.fetchall()]
+        
+    return rows
+    
+
+def extract_code(text):
+    pattern = r'```(.*?)```'
+    match = re.search(pattern, text, re.DOTALL)
+    
+    return match.group(1).strip().strip('python').strip() if match else None
+
+sql_generator = LLMChain(
+    llm=ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0.1, max_tokens=2048), 
+    prompt=generate_sql_prompt
+)
+
+sql_rewriter = LLMChain(
+    llm=ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0.1, max_tokens=2048), 
+    prompt=sql_rewriter_prompt
+)
+
+summarizer = LLMChain(
+    llm=ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0.5, max_tokens=2048), 
+    prompt=summarizer_prompt
+)
+
+
+def answer_question(question, path_to_db, table_name):
+
+    schema = str(get_schema(path_to_db, 'steam_games'))
+    df_samples = query_database(path_to_db, 'SELECT * FROM steam_games LIMIT 3;')
+    samples = str([tuple(df_samples.columns.tolist())] + [row[1:] for row in df_samples.to_records().tolist()])
+    
+    sql_query = sql_generator.run({
+        'question': question, 
+        'columns': schema, 
+        'samples': samples, 
+        'table_name': table_name
+    })
+
+    db_response = query_database(path_to_db, sql_query)
+
+    if str(db_response) == QUERY_FAILED:
+        answer_raw = sql_rewriter.run({
+            'question': question, 
+            'columns': schema, 
+            'samples': samples, 
+            'table_name': table_name,
+            'query': sql_query
+        })
+        
+        sql_query = extract_code(answer_raw)
+        answer = query_database(path_to_db, sql_query)
+    else:
+        answer = db_response
+        
+    summarized_response = summarizer.run({
+        'question': question, 
+        'query': sql_query, 
+        'answer': answer
+    })
+
+    return {
+        'summary': summarized_response,
+        'sql_query': sql_query,
+        'answer': answer
+    }
+
+if __name__ == '__main__':
+
+    question = 'What year had the highest rated games on average?'
+    table_name = 'steam_games'
+    path_to_db = Path.cwd() / '..' / 'tests' / 'datasets' / 'steam_games.db'
+
+    results = answer_question(
+        question=question,
+        path_to_db=path_to_db, 
+        table_name=table_name 
+    )
+
+    print(results)
